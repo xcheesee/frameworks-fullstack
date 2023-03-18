7a0724b5e7c03e0f93e4efb91c6f99ebfb0e9339
dorival <liscaDoido@gmail.com>
2023-03-17 22:11:51 -0300

    styling

--- server.py
+++ server.py
@@ -4,22 +4,15 @@ app = Flask(__name__) # Inicializa a aplicação
 
 @app.route('/') # Cria uma rota
 def main():
-  
-  #http://127.0.0.1:5000/?primeira=6&segunda=7
-  
-  parUm = request.args.get('primeira')
-  parDois = request.args.get('segunda')
   nota1 = request.args.get("nota1")
   nota2 = request.args.get("nota2")
-  stringBrabg = ""
   if nota1 is not None and nota2 is not None:
     nota1 = float(nota1)
     nota2 = float(nota2)
-    media = (nota1 + nota2) / 2
-    situacao = "reprovado" if  media < 5 else "aprovado" if media >= 7 else "dp"
-    stringBrabg = f'Nota 1: {str(nota1)} \n Nota 2: {str(nota2)} \n Media: {str((nota1 + nota2) / 2)} \n  Situacao: {situacao}'
+    media = (nota1 + nota2) / 2.0
+    situacao = "Reprovado" if  media < 5 else "Aprovado" if media >= 7 else "DP"
 
-  return render_template("index.html", stringBrabg=stringBrabg)
+  return render_template("index.html", nota1=nota1, nota2=nota2, situacao=situacao, media=media)
 
 if __name__ == '__main__':
   app.run() # Executa a aplicação
